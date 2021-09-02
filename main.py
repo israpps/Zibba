f6027311b4e34259e0a686c4e33ac476da2a13c3
Elisraa <>
2021-09-02 01:02:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+keep_alive()
+client.run(os.environ['TOKEN'])
